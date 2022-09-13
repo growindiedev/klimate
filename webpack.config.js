9e7da73b3315a8fe76f31f4dcc4ef399bfaad6af
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-09-13 15:14:38 +0530

    initialise src files and configuration

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Development",
+      template: "src/index.html",
     }),
     // using this plugin to automatically generate index.html
   ],
