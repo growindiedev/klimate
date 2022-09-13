7919b6bbaa645bfd1d80282fd6bd0aa0dc695eca
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-09-13 14:49:43 +0530

    initialise webpack and packages

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map", // figures out root file of the error
+  devServer: {
+    static: "./dist", //rebuilds when anything changes
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+    // using this plugin to automatically generate index.html
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, //cleans the old files in dist dir on each build
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
